bcf43e8034716cc03464cc33465bbf4ee033693c
ujjalmanshakya864 <shakyaujjalman@gmail.com>
2025-12-16 18:17:20 +0545

    test pipeline 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from Node.js');
+    res.end('Hello World from Node.js test test');
 });
 
 const PORT = 3001;
